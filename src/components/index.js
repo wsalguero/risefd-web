3caf2cdef57c47e3ba1e7934fffa360df11d60bd
williamsalguero <wesito2004@gmail.com>
2024-10-14 21:30:40 -0600

    Cleaning up main

--- src/components/index.js
+++
@@ -1,5 +0,0 @@
-export { default as Nav } from "./nav/nav";
-export { default as CardWithText } from "./cardWithText/cardWithText";
-export { default as CustomCarousel } from "./carousel/carousel";
-
-export * from "./buttons";
